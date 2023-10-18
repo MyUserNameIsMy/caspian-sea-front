ba5ad190fe1ce0a1068d2ea696a460b91c232a7a
myusernameismy <ilyas123.822@gmail.com>
2023-10-18 18:43:28 +0600

    change constant

--- src/common/constants/api.url.ts
+++ src/common/constants/api.url.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:8080/api/forecast/';
+export const BASE_URL = 'http://45.159.250.142:8080/api/forecast/';
+//export const BASE_URL = 'http://localhost:8080/api/forecast/';
